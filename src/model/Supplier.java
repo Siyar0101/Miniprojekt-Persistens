04385b57151ad2ff89efcb171aedf1e7d693e6a8
Siyar0101 <10700504@ucn.dk>
2026-03-11 10:45:08 +0100

    Supplier

--- src/model/Supplier.java
+++ src/model/Supplier.java
@@ -2,4 +2,38 @@ package model;
 
 public class Supplier {
 
+	private String name;
+	private String address;
+	private String Country;
+	private String phoneNo;
+	private String email;
+	
+	public Supplier(String name, String address, String Country, String phoneNo, String email) {
+		this.name = name;
+		this.address = address;
+		this.Country = Country;
+		this.phoneNo = phoneNo;
+		this.email = email;
+	}
+	
+	public String getname() {
+		return name;
+	}
+	
+	public String getaddress() {
+		return address;
+	}
+	
+	public String getCountry() {
+		return Country;
+	}
+	
+	public String getphoneNo() {
+		return phoneNo;
+	}
+	
+	public String getemail() {
+		return email;
+	}
+	
 }
