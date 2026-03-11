8e7bddb3e73847332edf85d751cda42fae40b6f6
Andreas <10700467@ucn.dk>
2026-03-11 11:17:35 +0100

    Update connect.java

--- src/db/connect.java
+++ src/db/connect.java
@@ -8,7 +8,7 @@ public class connect {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             Connection con = DriverManager.getConnection("jdbc:sqlserver://hildur.ucn.dk;"
-                    + "databaseName=DMA-CSD-S251_10435687;user=DMA-CSD-S251_10435687;password=Password1!;encrypt=false");
+                    + "databaseName=DMA-CSD-S251_10700467;user=DMA-CSD-S251_10700467;password=Password1!;encrypt=false");
             System.out.println("Connected to database: " + !con.isClosed()); 
             con.close();
             System.out.println("Connected to database: " + !con.isClosed()); 
