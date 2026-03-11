95bfee33d13c70cd152f8193a79a9d8c72e1d076
Benjii00986 <10435687@ucn.dk>
2026-03-11 10:26:40 +0100

    Create connect.java

---
+++ src/db/connect.java
@@ -0,0 +1,19 @@
+package db;
+
+import java.sql.*;
+
+public class connect {
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Connection con = DriverManager.getConnection("jdbc:sqlserver://hildur.ucn.dk;"
+                    + "databaseName=DMA-CSD-S251_10435687;user=DMA-CSD-S251_10435687;password=Password1!;encrypt=false");
+            System.out.println("Connected to database: " + !con.isClosed()); 
+            con.close();
+            System.out.println("Connected to database: " + !con.isClosed()); 
+        }    catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
