ac9eca72a9668ce0683438b92f47a1d3484915db
Benjii00986 <10435687@ucn.dk>
2026-03-11 10:53:54 +0100

    Update Customer.java

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,54 @@
 package model;
 
+
 public class Customer {
 
-}
+	private String name; 
+	private String address;
+	private int zipcode;
+	private String city;
+	private String phoneNo; 
+	private String email;
+	private String customerType;
+
+
+	public Customer(String name, String address, int zipcode, String city, String phoneNo, String email,
+			String customerType) {
+		this.name = name;
+		this.address = address;
+		this.zipcode = zipcode;
+		this.city = city;
+		this.phoneNo = phoneNo;
+		this.email = email;
+		this.customerType = customerType;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public int getZipcode() {
+		return zipcode;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getPhoneNo() {
+		return phoneNo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getCustomerType() {
+		return customerType;
+	}
+} 
+
