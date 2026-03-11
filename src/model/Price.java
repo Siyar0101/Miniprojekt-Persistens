db900343c66a4dc607cece948d74a2a48fa65ca6
Benjii00986 <10435687@ucn.dk>
2026-03-11 11:00:26 +0100

    Update Price.java

--- src/model/Price.java
+++ src/model/Price.java
@@ -1,14 +1,19 @@
 import java.time.LocalDate;
 
 public class Price {
-    private double price;
-    private LocalDate date;
+	private double price;
+	private LocalDate date;
 
-    public Price(double price, LocalDate date) {
-        this.price = price;
-        this.date = date;
-    }
+	public Price(double price, LocalDate date) {
+		this.price = price;
+		this.date = date;
+	}
 
-    public double getPrice() { return price; }
-    public LocalDate getDate() { return date; }
+	public double getPrice() {
+		return price;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
 }
